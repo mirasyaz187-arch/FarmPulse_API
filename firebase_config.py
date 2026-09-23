de0592c9ab7ebe2acc3581878d93fa32e5342fcd
Nazmira syazwin <mirasyaz187@gmail.com>
2026-09-23 21:31:44 +0800

    Update Firebase config to use JSON from environment
    Refactor Firebase initialization to use environment variable for credentials.

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,12 @@
+import os
+import json
 import firebase_admin
-import streamlit as st
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    firebase_secrets = dict(st.secrets["firebase"])
+    firebase_json = os.environ["FIREBASE_CREDENTIALS_JSON"]
+    firebase_secrets = json.loads(firebase_json)
+
     cred = credentials.Certificate(firebase_secrets)
     firebase_admin.initialize_app(cred)
 
